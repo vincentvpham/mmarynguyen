fb4082f705f12fd3f8ca88b08d3b7cc19855b7e2
Vincent Pham <vincentvpham@gmail.com>
2018-08-30 09:25:13 -0700

    Navigation init

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { Link } from 'gatsby'
+
+import './Navigation.scss'
+
+const Navigation = ({ title }) => {
+
+  return (
+    <div className='navigation'>
+      <Link to='/'>
+        <div className='title'>
+            {title}
+        </div>
+      </Link>
+      <div className='menu'>
+        <Link to='/about'>
+          About
+        </Link>
+        <Link to='/involvement'>
+          Involvement
+        </Link>
+        <Link to='/'>
+          Artwork
+        </Link>
+        <Link to='/contant'>
+          Contact
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Navigation
