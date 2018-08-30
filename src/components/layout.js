8b54c6856588bb4475f0b9e197cd53c3efa41e1f
Vincent Pham <vincentvpham@gmail.com>
2018-08-30 09:25:56 -0700

    adjust layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,8 +3,10 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
-import Header from './header'
+import Navigation from '../components/Navigation/Navigation';
+
 import './layout.css'
+import './layout.scss'
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -18,7 +20,7 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
+      <div className='layout'>
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
@@ -28,18 +30,11 @@ const Layout = ({ children }) => (
         >
           <html lang="en" />
         </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
+        <Navigation title={data.site.siteMetadata.title} />
+        <div>
           {children}
         </div>
-      </>
+      </div>
     )}
   />
 )
