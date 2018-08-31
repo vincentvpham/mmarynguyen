aebbd901a109065311ad35884b408c516598deba
Vincent Pham <vincentvpham@gmail.com>
2018-08-31 09:22:45 -0700

    /involvement

--- src/pages/involvement.js
+++ src/pages/involvement.js
@@ -2,9 +2,53 @@ import React from 'react'
 
 import Layout from '../components/layout'
 
+import '../styles/involvement.scss'
+
 const InvolvementPage = () => (
   <Layout>
-    <h1>Involvement</h1>
+    <div className='involvement'>
+      <h1>Involvement</h1>
+      <div className='content'>
+        <ul>
+          <li>
+            RESEARCH ASSISTANT - <b>UMKC DEPARTMENT OF PSYCHOLOGY</b> // (2018-present)
+          </li>
+          <li>
+            EMERGENCY DEPARTMENT VOLUNTEER - <b>ST. LUKES HOSPITAL</b> // (2017-present)
+          </li>
+          <li>
+            ​SALES ASSOCIATE - <b>GAP</b> // (2016-2017)
+          </li>
+          <li>
+            YE AMBASSADOR - <b>YOUTH ENTREPRENEURS</b> // (2015-2016)
+          </li>
+          <li>
+            GRAPHIC DESIGNER - <b>FREESTYLE SIGNS CO.</b> // (2013-2016)
+          </li>
+          <li>
+            --
+          </li>
+          <li>
+            VICE-PRESIDENT - <b>UMKC PRE-MEDICAL SOCIETY</b>
+          </li>
+          <li>
+            SOCIAL MEDIA COORDINATOR - <b>UMKC PLUS ENERGY</b>
+          </li>
+          <li>
+            MEMBER - <b>UMKC MINORITY ASSOCIATION OF PRE-MEDICAL STUDENTS</b>
+          </li>
+          <li>
+            MEMBER - <b>UMKC GLOBE MED</b>
+          </li>
+          <li>
+            MEMBER - <b>UMKC BIOLOGICAL STUDENTS GOVERNMENT ASSOCIATION</b>
+          </li>
+          <li>
+            MEMBER - <b>UMKC UNION PROGRAMMING BOARD</b>
+          </li>
+        </ul>
+      </div>
+    </div>
   </Layout>
 )
 
