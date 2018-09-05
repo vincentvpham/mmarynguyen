0cd980cd347c4c8ec4501fa3323a9b6d71d7e899
Vincent Pham <vincentvpham@gmail.com>
2018-09-05 09:32:01 -0700

    contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,9 +2,61 @@ import React from 'react'
 
 import Layout from '../components/layout'
 
+import '../styles/contact.scss';
+
 const ContactPage = () => (
   <Layout>
-    <h1>Contact</h1>
+    <div className='contact'>
+      <h1>Contact</h1>
+      <div className='contact-methods'>
+        <div className='method'>
+          <div className='platform'>
+            E-MAIL
+          </div>
+          <div className='value'>
+            MARYNGUYEN@REBORN.COM
+          </div>
+        </div>
+        <div className='method'>
+          <div className='platform'>
+            INSTAGRAM
+          </div>
+          <div className='value'>
+            WWW.INSTAGRAM.COM/MMARYNGUYENN
+          </div>
+        </div>
+        <div className='method'>
+          <div className='platform'>
+            DEPOP
+          </div>
+          <div className='value'>
+            WWW.DEPOP.COM/MMARYNGUYEN
+          </div>
+        </div>
+        <div className='method'>
+          <div className='platform'>
+            ETSY
+          </div>
+          <div className='value'>
+            WWW.ETSY.COM/PEOPLE/MMARYNGUYEN
+          </div>
+        </div>
+      </div>
+
+      <div className='currently'>
+        Mary is currently residing in Kansas City, MO.
+      </div>
+
+      <div className='map'>
+        <iframe
+          title='currently'
+          width='100%'
+          height='300'
+          frameBorder='0'
+          src='//www.google.com/maps/embed/v1/place?key=AIzaSyBadiCPcXU36skJ3jMufYRFcvV7vQzIgUc&q=Kansas+City+MO'
+          allowFullScreen />
+      </div>
+    </div>
   </Layout>
 )
 
