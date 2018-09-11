b2e9c53162a46c386101732999e64224e9c1dfb1
Vincent Pham <vincentvpham@gmail.com>
2018-09-11 09:31:11 -0700

    Artwork layout

---
+++ src/layouts/Artwork/Artwork.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import { Link, navigateTo } from 'gatsby'
+
+import Layout from '../../components/layout'
+
+import './Artwork.scss'
+
+const Artwork = ({ children }) => {
+
+  const _goToArtworkIndex = () => navigateTo('/artwork')
+
+  return (
+    <Layout>
+      <div className='artwork'>
+        <h1 onClick={_goToArtworkIndex}>Artwork</h1>
+        <div className='artwork-nav'>
+          <Link to='/artwork/drawings' activeClassName='active'>
+            Drawings
+          </Link>
+          <Link to='/artwork/paintings' activeClassName='active'>
+            Paintings
+          </Link>
+          <Link to='/artwork/photography' activeClassName='active'>
+            Photography
+          </Link>
+        </div>
+        <div className='gallery'>
+          {children}
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default Artwork
