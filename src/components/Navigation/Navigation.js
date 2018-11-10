5a9b8914cdab9806cb2e621aadef23ce9f4eff13
Vincent Pham <vincentvpham@gmail.com>
2018-11-10 14:58:24 -0800

    factor Menu out of Navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { Link } from 'gatsby'
+import Menu from './Menu/Menu';
 
 import './Navigation.scss'
 
@@ -9,28 +9,7 @@ const Navigation = ({ title }) => {
 
   return (
     <div className='navigation'>
-      <Link to='/'>
-        <div className='title'>
-            {title}
-        </div>
-      </Link>
-      <div className='menu'>
-        <Link to='/about' activeClassName='active'>
-          About
-        </Link>
-        <Link to='/involvement' activeClassName='active'>
-          Involvement
-        </Link>
-        <Link to='/artwork' activeClassName='active'>
-          Artwork
-        </Link>
-        <Link to='/contact' activeClassName='active'>
-          Contact
-        </Link>
-        <a href='https://www.depop.com/mmarynguyen' target='_blank' rel="noopener noreferrer">
-          Shop
-        </a>
-      </div>
+      <Menu title={title} />
     </div>
   )
 }
