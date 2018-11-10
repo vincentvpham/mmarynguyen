c139786b4e777d7ea7f7f70ceff3a565c88371ef
Vincent Pham <vincentvpham@gmail.com>
2018-11-10 15:14:55 -0800

    Involvement - responsive edit

--- src/pages/involvement.js
+++ src/pages/involvement.js
@@ -11,40 +11,56 @@ const InvolvementPage = () => (
       <div className='content'>
         <ul>
           <li>
-            RESEARCH ASSISTANT - <b>UMKC DEPARTMENT OF PSYCHOLOGY</b> {'//'} (2018-present)
+            <span className='where'>UMKC DEPARTMENT OF PSYCHOLOGY</span>
+            <span className='what'>RESEARCH ASSISTANT</span>
+            <span className='when'>2018-present</span>
           </li>
           <li>
-            EMERGENCY DEPARTMENT VOLUNTEER - <b>ST. LUKES HOSPITAL</b> {'//'} (2017-present)
+            <span className='where'>ST. LUKES HOSPITAL</span>
+            <span className='what'>EMERGENCY DEPARTMENT VOLUNTEER</span>
+            <span className='when'>2017-present</span>
           </li>
           <li>
-            ​SALES ASSOCIATE - <b>GAP</b> {'//'} (2016-2017)
+            <span className='where'>GAP</span>
+            <span className='what'>SALES ASSOCIATE</span>
+            <span className='when'>2016-2017</span>
           </li>
           <li>
-            YE AMBASSADOR - <b>YOUTH ENTREPRENEURS</b> {'//'} (2015-2016)
+            <span className='where'>YOUTH ENTREPRENEURS</span>
+            <span className='what'>YE AMBASSADOR</span>
+            <span className='when'>2015-2016</span>
           </li>
           <li>
-            GRAPHIC DESIGNER - <b>FREESTYLE SIGNS CO.</b> {'//'} (2013-2016)
+            <span className='where'>FREESTYLE SIGNS CO.</span>
+            <span className='what'>GRAPHIC DESIGNER</span>
+            <span className='when'>2013-2016</span>
           </li>
           <li>
-            --
+          ---
           </li>
           <li>
-            VICE-PRESIDENT - <b>UMKC PRE-MEDICAL SOCIETY</b>
+            <span className='where'>UMKC PRE-MEDICAL SOCIETY</span>
+            <span className='what'>VICE-PRESIDENT</span>
           </li>
           <li>
-            SOCIAL MEDIA COORDINATOR - <b>UMKC PLUS ENERGY</b>
+            <span className='where'>UMKC PLUS ENERGY</span>
+            <span className='what'>SOCIAL MEDIA COORDINATOR</span>
           </li>
           <li>
-            MEMBER - <b>UMKC MINORITY ASSOCIATION OF PRE-MEDICAL STUDENTS</b>
+            <span className='where'>UMKC MINORITY ASSOCIATION OF PRE-MEDICAL STUDENTS</span>
+            <span className='what'>MEMBER</span>
           </li>
           <li>
-            MEMBER - <b>UMKC GLOBE MED</b>
+            <span className='where'>UMKC GLOBE MED</span>
+            <span className='what'>MEMBER</span>
           </li>
           <li>
-            MEMBER - <b>UMKC BIOLOGICAL STUDENTS GOVERNMENT ASSOCIATION</b>
+            <span className='where'>UMKC BIOLOGICAL STUDENTS GOVERNMENT ASSOCIATION</span>
+            <span className='what'>MEMBER</span>
           </li>
           <li>
-            MEMBER - <b>UMKC UNION PROGRAMMING BOARD</b>
+            <span className='where'>UMKC UNION PROGRAMMING BOARD</span>
+            <span className='what'>MEMBER</span>
           </li>
         </ul>
       </div>
