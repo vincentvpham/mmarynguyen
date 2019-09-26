0c85762ed4e4f587e0d3255a1a40db7abaa7b05c
Vincent Pham <vincentvpham@gmail.com>
2019-09-26 08:49:32 -0700

    about

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,30 +2,25 @@ import React from 'react'
 
 import Layout from '../components/layout'
 
-import Banner from '../components/Banner/Banner'
-
-import aboutBannerImage from '../images/about.jpg'
+import aboutImage from '../images/about.png'
 
 import '../styles/about.scss'
 
 const AboutPage = () => (
   <Layout>
-    <div className='about'>
-      <Banner
-        image={aboutBannerImage}
-      />
+    <div className="about">
+      <img src={aboutImage} alt="about mary nguyen" />
       <div>
         <h1>About Mary</h1>
-        <div className='description'>
-          Born and raised in Wichita, KS, Mary Nguyen is a student, artist and entrepreneur.
-          She is currently attending the University of Missouri-Kansas City where she is earning
-          her Bachelor of Science in Biology and minoring in Chemistry and Psychology.
-          While actively involved in multiple school organizations and keeping up with schoolwork,
-          Mary makes time for her other interests such as creating artwork, playing music and
-          reading books. With her interests in the arts, Mary launched Out of Line Apparel in 2018
-          which features her artwork on clothing, prints and stickers. Intrigued by human behavior,
-          motivation and the biological aspects of psychology, she plans on continuing her education
-          by attending medical school.
+        <div className="description">
+          Born and raised in Wichita, KS, Mary Nguyen is a student, artist and
+          entrepreneur. She is currently attending the University of
+          Missouri-Kansas City where she is earning her Bachelor of Science in
+          Biology and minoring in Chemistry and Psychology. She is the founder
+          of Out of Line Apparel which features her artwork on clothing, prints
+          and stickers. Intrigued by human behavior, motivation and the
+          biological aspects of psychology, she hopes to continue her education
+          by attending graduate school.
         </div>
       </div>
     </div>
