51f639aaae0cb7878116e8cecc97267a545c8a9d
Vincent Pham <vincentvpham@gmail.com>
2022-04-06 23:07:01 -0700

    update navigation

--- src/components/Navigation/Menu/Menu.js
+++ src/components/Navigation/Menu/Menu.js
@@ -1,36 +1,60 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faLinkedinIn,
+  faInstagram,
+  faFacebookF,
+  faTiktok,
+} from '@fortawesome/free-brands-svg-icons';
 
-import { Link } from 'gatsby'
+import { Link } from 'gatsby';
 
-import './Menu.scss'
+import logo from './mn.png';
+
+import './Menu.scss';
 
 const Menu = ({ title }) => {
   return (
     <div className="menu">
-      <Link to="/">
-        <div className="title">{title}</div>
-      </Link>
-      <div className="routes">
-        <Link to="/about" activeClassName="active">
-          About
+      <div className="body">
+        <img src={logo} alt="mary nguyen logo" />
+        <Link to="/">
+          <div className="title">{title}</div>
         </Link>
-        <Link to="/cv" activeClassName="active">
-          CV
-        </Link>
-        <Link to="/artwork" activeClassName="active">
-          Artwork
-        </Link>
-        <Link to="/contact" activeClassName="active">
-          Contact
-        </Link>
+
+        <div className="routes">
+          <Link to="/about" activeClassName="active">
+            About
+          </Link>
+          <Link to="/cv" activeClassName="active">
+            CV
+          </Link>
+          <Link to="/artwork" activeClassName="active">
+            Artwork
+          </Link>
+          <Link to="/contact" activeClassName="active">
+            Contact
+          </Link>
+        </div>
+      </div>
+      <div className="footer">
+        <div>
+          <FontAwesomeIcon className="social-media" icon={faLinkedinIn} />
+          <FontAwesomeIcon className="social-media" icon={faInstagram} />
+          <FontAwesomeIcon className="social-media" icon={faFacebookF} />
+          <FontAwesomeIcon className="social-media" icon={faTiktok} />
+        </div>
+        <div className="copyright">
+          &copy; copyright {new Date().getFullYear()}, mary nguyen
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 Menu.propTypes = {
   title: PropTypes.string.isRequired,
-}
+};
 
-export default Menu
+export default Menu;
